d8f90810594bbf2d1b1033634d26e9c7581d33b1
Rahman <museabdirahman@gmail.com>
2025-03-14 11:41:02 +0000

    Implemented basic CRUD architecture

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+
+//this file will seed our database with new jitz camp data ~50 records before clearing the db
+
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+  const cities = require('./cities')
+  const {places,descriptors} = require('./seedHelpers')
+
+
+
+mongoose.connect('mongodb://localhost:27017/jitz-camp')
+
+// check
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"connection error:"))
+db.once("open",()=>{
+  console.log("DB connected!")
+})
+
+
+// clear db evertime you seed/populate db cos of changes to model
+const sample = array=>array[Math.floor(Math.random()*array.length)]
+
+
+const seedDb= async ()=>{
+  await Campground.deleteMany({})
+  for(let i=0;i<50;i++){
+    const random1000 = Math.floor(Math.random()*1000);
+   const newGround= new Campground({
+      title:`${sample(descriptors)} ${sample(places)}`,
+      location:`${cities[random1000].city}, ${cities[random1000].state}`
+    })
+    await newGround.save()
+  }
+}
+
+seedDb().then(()=>{
+  mongoose.connection.close()
+})
---
+++ seeds/seedHelpers.js
@@ -0,0 +1,43 @@
+module.exports.descriptors = [
+  'Gracie',
+  'Alliance',
+  'Checkmat',
+  'Atos',
+  '10th Planet',
+  'Nova Uniao',
+  'GF Team',
+  'Carlson Gracie',
+  'Zenith',
+  'Soul Fighters',
+  'Fight Sports',
+  'Unity',
+  'Art of Jiu Jitsu',
+  'Tri-Force',
+  'Ribeiro',
+  'BTT',
+  'De La Riva'
+]
+
+module.exports.places = [
+  'Academy',
+  'Dojo',
+  'Training Center',
+  'Gym',
+  'HQ',
+  'School',
+  'Mat Club',
+  'Fight Club',
+  'Camp',
+  'Studio',
+  'Temple',
+  'House',
+  'War Room',
+  'Lab',
+  'Fort',
+  'Arena',
+  'Sanctuary',
+  'Base',
+  'Barn',
+  'Den'
+]
+
